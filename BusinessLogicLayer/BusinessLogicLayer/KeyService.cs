c30be987b16bf5f0da4a78f4a69a79b14441916c
victor <victor.quintana@dwellant.com>
2017-08-29 15:10:04 +0100

    Dwellant Test

---
+++ BusinessLogicLayer/BusinessLogicLayer/KeyService.cs
@@ -0,0 +1,13 @@
+﻿namespace BusinessLogicLayer
+{
+    public class KeyService
+    {
+        // read a csv file and write the the total of times that a key was borrowed
+        // Expected output:
+        // 888, 1
+        // 1234, 5
+        // 5555, 2
+        
+
+    }
+}
---
+++ BusinessLogicLayer/BusinessLogicLayer/Utils/TextFileReader.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace BusinessLogicLayer.Utils
+{
+    public class TextFileReader
+    {
+        public string Read(string filePath)
+        {
+            var list = new List<string>();
+
+            using (var reader = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    list.Add(line);
+                }
+            }
+
+            return list.ToString();
+        }
+
+        public void Write(string filePath, string fileContent)
+        {
+            using (var file = new StreamWriter(filePath))
+            {
+                file.Write(fileContent);
+            }
+        }
+
+        public string[] ReadCsv(string filePath)
+        {
+            var results = new List<string>();
+
+            using (var reader = new StreamReader(filePath))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+
+                    for (var index = 0; index < values.Length; index++)
+                    {
+                        var item = values[index];
+                        results.Add(item);
+                    }
+                }
+            }
+
+            return results.ToArray();
+        }
+    }
+}
